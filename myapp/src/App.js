d1204b9d0fabd781634f3aab891080918748caa7
xiejun <1073850546@qq.com>
2020-02-12 20:58:39 +0800

    commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
-
+import {Route, BrowserRouter as Router,Switch} from 'react-router-dom'
+import Login from './pages/Login'
+import Admin from './pages/Admin'
 function App() {
   return (
-    <div >
-      app
-    </div>
+     <Router>
+        <Switch>
+            <Route path='/login' exact component={Login}></Route>
+            <Route path='/' component={Admin}></Route>
+        </Switch>
+     </Router>
   );
 }
 
---
+++ myapp/src/pages/Admin/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Layout from '../../component/Layout'
+export default class Admin extends Component {
+    render() {
+        return (
+            <Layout>
+                
+            </Layout>
+        )
+    }
+}
